30b4e8c5ba87da7778d609f9e4119b6799af6f6f
Pang <chzmzx@gmail.com>
2023-03-29 14:28:05 +0700

    p1

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace costomname{
+          class Program{
+                    static void Main(string[] args){
+                         Console.Write("input: ");
+                         int number = int.Parse(Console.ReadLine());
+                         int N = int.Parse(Console.ReadLine());
+                         int K = int.Parse(Console.ReadLine());
+
+                         for (int row = 1; row <= 3; ) {
+                            for (int col = 1; col > 0; ) {
+                            }
+                            Console.WriteLine();     
+                         }
+
+                         
+                    }
+          }
+}
